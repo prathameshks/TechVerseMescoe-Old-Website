f15274ed304f1f8b21b591e9522d77ab88a4f400
Kaustubh-Warade <kaustubhwarade@gmail.com>
2023-04-27 20:36:53 +0530

    mail

--- assets/php/submit_contact.php
+++ assets/php/submit_contact.php
@@ -43,7 +43,45 @@ if ($name === "" or $name === " ") {
     $res['code'] = 'success';
     $res['desc'] = 'success';
 
-    
+    //sending order mail
+    function send_order($to_mail, $to_name, $order_id, $cart_json, $cut, $total_cost, $mobile, $room, $del_loc)
+    {
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = 0;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                   //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'techverse.mescoe@gmail.com';                     //SMTP username
+        $mail->Password   = 'znykpkkahixwlzze';                               //SMTP password
+        $mail->SMTPSecure = 'ssl';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->Port       = 465;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+        //Recipients
+        $mail->setFrom('techverse.mescoe@gmail.com', 'TechVerse');
+        $mail->addAddress($to_mail, $to_name);     //Add a recipient
+        $mail->addBCC('techverse.mescoe@gmail.com', 'Admin');     //Add a recipient
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = "TechVerse Event Registration Confirmation>";
+        $mailBody = "
+            
+            ";
+        // $mail->Body    = $mailBody;
+
+        // $mail->AltBody = $mailBody;
+
+        $mail->send();
+        return true;
+    } catch (Exception $e) {
+        return false;
+        // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+    }   
+
 }
 
 echo json_encode($res);
